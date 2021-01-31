c8fe72f127c4791c795379b27ee20e941704fad4
tsepulya <tsepeleva_anna@mail.ru>
2021-01-31 20:31:43 +0300

    fix: for

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@
 
 module.exports = function towelSort (matrix) {
   let array = [];
-    if (matrix === undefined || matrix.length === 0) {
-        return array;
-    } else {
-       return matrix.join(',').split(',').map(a => Number(a));
-    }
+  if (matrix === undefined || matrix.length === 0) {
+      return array;
+  } else {
+     for (let i = 0; i < matrix.length; i ++) {
+         for (let j = 0; j < matrix[i].length; j++) {
+             array.push(matrix[i][j]);
+         }
+     }
+     return array;
+  }
 }
