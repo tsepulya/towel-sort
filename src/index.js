494277faa1fb8cc961b763d33cda4b7f1cb6852a
tsepulya <tsepeleva_anna@mail.ru>
2021-01-31 20:02:29 +0300

    fix: empty

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return matrix.join(',').split(',').map(a => Number(a));
+  let array = [];
+    if (matrix === undefined || matrix.length === 0) {
+        return array;
+    } else {
+       return matrix.join(',').split(',').map(a => Number(a));
+    }
 }
