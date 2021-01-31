ae20f1d531dd7b9e16a799e7d4025d8b645911bc
tsepulya <tsepeleva_anna@mail.ru>
2021-01-31 19:55:02 +0300

    fix: change function

--- src/index.js
+++ src/index.js
@@ -2,11 +2,5 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  let result = [];
-  for (let i = 0; i < matrix.length; i++) {
-      for (let k = 0; k < matrix[i].length; k++) {
-          result.push(matrix[i][k]);
-      }
-  }
-  return result;
+  return matrix.join(',').split(',').map(a => Number(a));
 }
